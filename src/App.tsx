c14f7b93cbcd708d2354432d4b0bf39455c5b43f
Michael Rocha <michaelrocha9722@gmail.com>
2026-03-20 16:26:33 -0300

    Init project with vite, add images and install dependencies.
    - init vite with typescript
    - install dependencies
    - add svg
    - add image

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <div className="text-red-500">Hello World</div>,
+	},
+]);
+
+function App() {
+	return (
+		<>
+			<RouterProvider router={router} />
+		</>
+	);
+}
+
+export default App;
