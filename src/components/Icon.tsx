0654226c7d759cde081e54a73eb5ffdb12fbee13
Michael Rocha <michaelrocha9722@gmail.com>
2026-03-24 15:08:45 -0300

    add svg component

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -1,11 +1,22 @@
-import type { SVGProps } from "react" 
+import type { FC, SVGProps } from "react";
 
-type IconProps extends SVGProps= {}
-
-function Icon({}: Props) {
-  return (
-    <div>Icon</div>
-  )
+// Definimos o tipo das props: aceita um "SvgComponent" e as props normais de SVG
+interface IconProps extends SVGProps<SVGSVGElement> {
+	// Aqui você usa a assinatura de um componente React que renderiza SVG
+	component: FC<SVGProps<SVGSVGElement>>;
+	fill: string;
+	className: string;
 }
 
-export default Icon
+const Icon = ({ component: SvgComponent, fill = "#000000", className, ...props }: IconProps) => {
+	return (
+		<SvgComponent
+			fill={fill}
+			// O 'fill="currentColor"' permite que o ícone herde a cor do texto do elemento pai
+			className={className}
+			{...props}
+		/>
+	);
+};
+
+export default Icon;
