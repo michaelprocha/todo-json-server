147eef800bae26059caf35583894894d6604eead
Michael Rocha <michaelrocha9722@gmail.com>
2026-04-09 16:35:32 -0300

    add PlusIcon and CheckIcon component

---
+++ src/components/icons/PlusIcon.tsx
@@ -0,0 +1,39 @@
+import type { SVGProps } from "react";
+import { twMerge } from "tailwind-merge";
+import { tv, type VariantProps } from "tailwind-variants";
+
+const plusIconVariants = tv({
+	variants: {
+		color: {
+			blueDark: "text-blue-dark",
+		},
+		size: {
+			md: "size-4",
+		},
+	},
+	defaultVariants: {
+		color: "blueDark",
+		size: "md",
+	},
+});
+
+type PlusIconProps = VariantProps<typeof plusIconVariants> &
+	SVGProps<SVGSVGElement> & {
+		className?: string;
+	} & Omit<SVGProps<SVGSVGElement>, keyof VariantProps<typeof plusIconVariants>>;
+
+function PlusIcon({ color, size, className, ...props }: PlusIconProps) {
+	return (
+		<svg
+			viewBox="0 0 16 16"
+			fill="currentColor"
+			xmlns="http://www.w3.org/2000/svg"
+			className={twMerge(plusIconVariants({ color, size }), className)}
+			{...props}
+		>
+			<path d="M6.275 9.425H0V6.275H6.275V0H9.425V6.275H15.7V9.425H9.425V15.7H6.275V9.425Z" />
+		</svg>
+	);
+}
+
+export default PlusIcon;
