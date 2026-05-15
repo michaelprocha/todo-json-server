f208bc856e22ea65b2408f12aa3d129dd73bbc6a
Michael Rocha <michaelrocha9722@gmail.com>
2026-05-15 14:10:13 -0300

    test delete task

--- src/hooks/useTasks.test.ts
+++ src/hooks/useTasks.test.ts
@@ -47,18 +47,65 @@ describe("useTasks", () => {
       await result.current.addTask("New task");
     });
 
-    await waitFor(() => {
-      expect(result.current.tasks).toHaveLength(1);
-    });
+    await waitFor(() => expect(result.current.isPading).toBe(false));
+
+    expect(result.current.tasks).toHaveLength(1);
 
     expect(result.current.tasks[0]).toMatchObject({
       content: "New task",
       completed: false,
+      id: "123",
     });
     expectTypeOf(result.current.tasks[0]).toEqualTypeOf<{
       content: string;
       completed: boolean;
       id: string;
     }>();
+  });
+
+  it("Delete a task", async () => {
+    const fetchMock = vi
+      .fn()
+      .mockResolvedValueOnce({
+        ok: true,
+        json: async () => [],
+      } as Response)
+      .mockResolvedValueOnce({
+        ok: true,
+        json: async () => MOCK_TASK,
+      } as Response)
+      .mockResolvedValueOnce({
+        ok: true,
+      } as Response);
+
+    vi.stubGlobal("fetch", fetchMock);
+
+    const { result } = renderHook(() => useTasks());
+
+    waitFor(() => expect(result.current.isPading).toBe(false));
+
+    await waitFor(async () => await result.current.addTask("New task"));
+
+    await waitFor(() => expect(result.current.isPading).toBe(false));
+
+    expect(result.current.tasks).toHaveLength(1);
+
+    expect(result.current.tasks[0]).toEqual({
+      content: "New task",
+      completed: false,
+      id: "123",
+    });
+
+    expectTypeOf(result.current.tasks[0]).toEqualTypeOf<{
+      content: string;
+      completed: boolean;
+      id: string;
+    }>();
+
+    await waitFor(async () => await result.current.deleteTask("123"));
+
+    await waitFor(() => expect(result.current.isPading).toBe(false));
+
+    expect(result.current.tasks).toHaveLength(0);
   });
 });
