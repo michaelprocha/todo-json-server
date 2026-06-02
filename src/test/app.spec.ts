7fd6dc66b22c27b245b7ca5f4512bbe50c370d43
Michael Rocha <michaelrocha9722@gmail.com>
2026-06-02 16:18:18 -0300

    test add tarefa playwright

--- src/test/app.spec.ts
+++ src/test/app.spec.ts
@@ -12,4 +12,20 @@ test.describe("home page", () => {
   test("check the title", async ({ page }) => {
     await expect(page).toHaveTitle("todo-json-server");
   });
+
+  test("check the heding", async ({ page }) => {
+    const hedingH1 = page.getByRole("heading", {
+      name: "Gerenciador de Produtividade",
+      level: 1,
+    });
+    await expect(hedingH1).toBeVisible();
+  });
+
+  test("add new task", async ({ page }) => {
+    const input = page.getByRole("textbox");
+    await input.fill("task123");
+    const addButton = page.getByRole("button");
+    await addButton.click();
+    await expect(page.getByText("task123")).toBeVisible();
+  });
 });
